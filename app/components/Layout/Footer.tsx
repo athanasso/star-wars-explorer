04ff1ae846a9c297cc2275453893c810d09728c1
Emmanouil Athanasopoulos <70025614+athanasso@users.noreply.github.com>
2024-11-21 13:11:08 +0200

    Component: Footer

---
+++ app/components/Layout/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+    <footer className="bg-gray-900 p-4 text-center">
+        <p>&copy; 2024 Star Wars Explorer. May the Force be with you!</p>
+    </footer>
+    );
+}
